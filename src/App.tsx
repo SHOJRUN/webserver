f6e9521b46b559aa6de5e56b1bdce5493cdfd6dd
shoji <uladzimir.kop@gmail.com>
2023-07-25 22:20:08 +0200

    Team name updated

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const teamName = 'Vova & Yura';
+  const teamName = 'Vova & Yura - super team';
 
   return (
     <>
